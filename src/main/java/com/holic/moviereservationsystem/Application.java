d8d11979959422914b66124655d98c46bcb87ae8
jisuk-ssi <kimsssuk319@gmail.com>
2026-08-13 23:28:28 +0900

    Create new movie reservation system

---
+++ src/main/java/com/holic/moviereservationsystem/Application.java
@@ -0,0 +1,9 @@
+package com.holic.moviereservationsystem;
+
+public class Application {
+
+    public static void main(String[] args) {
+
+        System.out.println("This is a movie reservation system!");
+    }
+}
